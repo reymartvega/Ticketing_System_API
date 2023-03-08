cda25209d055c059a6ff3d170117b612efea3937
reymartvega@gmail.com <reymartvega@gmail.com>
2023-03-08 16:11:03 +0800

    init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+//middlewares
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+app.use(morgan('common'))
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
+
+//database connection
+const PORT = process.env.PORT || 6000
+mongoose.set('strictQuery',true)
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+let db = mongoose.connection;
+
+// for error handling
+db.on("error",console.error.bind(console,"Connection Error!"))
+
+// for validation
+db.once("open",()=>{console.log("We are connected to the cloud!")})
+
+app.listen(PORT,() => console.log(`Connected at server port: ${PORT}`))
